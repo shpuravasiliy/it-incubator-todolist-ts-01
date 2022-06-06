eba4962990adcf73ec582a18239339bf31739d61
Shpura Vasiliy <shpura.vasiliy@gmail.com>
2022-06-06 18:07:20 +0300

    done 2nd lesson

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,50 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
+import TodoList, {TaskType} from './TodoList';
+
+export type FilterValuesType = 'all' | 'active' | 'completed';
 
 function App() {
+    //BLL - Business logic level
+    let [tasks, setTasks] = useState<Array<TaskType>>([
+        {id: 1, title: 'HTML&CSS', isDone: true},
+        {id: 2, title: 'JS/TS', isDone: false},
+        {id: 3, title: 'React', isDone: false}
+    ])
+
+    // let tasks: Array<TaskType> = [
+    //     {id: 1, title: 'HTML&CSS', isDone: true},
+    //     {id: 2, title: 'JS/TS', isDone: false},
+    //     {id: 3, title: 'React', isDone: false}
+    // ];
+
+    const [filter, setFilter] = useState<FilterValuesType>('all')
+
+    const removeTask = (taskID: number) => {
+        const filteredTasks = tasks.filter((t) => t.id !== taskID);
+        setTasks(filteredTasks);
+    }
+
+    const onClickHandler = (filter: FilterValuesType) => {
+        setFilter(filter);
+    }
+
+    let tasksForRender = tasks;
+    if (filter === 'active') {
+        tasksForRender = tasks.filter(t => t.isDone === false)
+    }
+    if (filter === 'completed') {
+        tasksForRender = tasks.filter(t => t.isDone === true)
+    }
+
     return (
         <div className="App">
-            <div>
-                <h3>What to learn</h3>
-                <div>
-                    <input/>
-                    <button>+</button>
-                </div>
-                <ul>
-                    <li><input type="checkbox" checked={true}/> <span>HTML&CSS</span></li>
-                    <li><input type="checkbox" checked={true}/> <span>JS</span></li>
-                    <li><input type="checkbox" checked={false}/> <span>React</span></li>
-                </ul>
-                <div>
-                    <button>All</button>
-                    <button>Active</button>
-                    <button>Completed</button>
-                </div>
-            </div>
+            <TodoList
+                title={'What to do'}
+                tasks={tasksForRender}
+                removeTask={removeTask}
+                onClickHandler={onClickHandler}
+            />
         </div>
     );
 }
